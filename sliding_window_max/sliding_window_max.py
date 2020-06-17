5e59bdd6d367831297f92c4be3a464ccb4234020
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-06-17 17:15:22 -0400

    finished slide

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,30 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
+    new_arr = []
+    start = 0
+    slide = k
+    for i in range(len(nums)):
+        high = float("-inf")
+        for j in range(start, slide):
+            if nums[j] > high:
+                high = nums[j]
+            if len(nums) < slide:
+                return new_arr
+        new_arr.append(high)
+        start += 1
+        slide += 1
+    return new_arr
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
